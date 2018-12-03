658db903f5bb48291cc9c12ef46a9a8404ad0f28
ctran843 <ctran843@gmail.com>
2018-12-02 23:12:19 -0800

    Add files via upload

--- mnist.py
+++ mnist.py
@@ -1,3 +1,9 @@
+"""
+Cole Watson, Chris Tran
+CS480
+Fall 2018
+"""
+
 
 # Code from Chapter 4 of Machine Learning: An Algorithmic Perspective (2nd Edition)
 # by Stephen Marsland (http://stephenmonika.net)
@@ -34,13 +40,11 @@ for i in range(len(tset[0])):
 seven_target_set = np.zeros((len(seven_image_set), 10))
 for i in range(len(seven_image_set)):
     seven_target_set[i][7] = 1
-print (seven_target_set[0])
 
 #labels for nine set
 nine_target_set = np.zeros((len(nine_image_set), 10))
 for i in range(len(nine_image_set)):
     nine_target_set[i][9] = 1
-print (nine_target_set[0])
 
 #TRAIN image set
 TRAIN_image_set = seven_image_set
@@ -50,14 +54,14 @@ for i in range(len(nine_image_set)):
 #TRAIN label set
 TRAIN_label_set = seven_target_set
 for i in range(len(nine_target_set)):
-    TRAIN_label_set.append(nine_target_set[i])
+    np.append(TRAIN_label_set, nine_target_set[i])
 
 
 nread = 200
 # Just use the first few images
 #training images
 train_in = tset[0][:nread, :]
-print(train_in[5])
+print(train_in[0])
 
 # This is a little bit of work -- 1 of N encoding
 # Make sure you understand how it does it
@@ -65,7 +69,6 @@ print(train_in[5])
 train_tgt = np.zeros((nread, 10))
 for i in range(nread):
     train_tgt[i, tset[1][i]] = 1
-print(train_tgt[0][5])
 
 #test images
 test_in = teset[0][:nread, :]
